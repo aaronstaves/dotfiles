8777e73076cd5ef517a480c9aec7186b839bc513
Aaron Staves <aaron.staves@streetlinks.com>
2015-05-20 11:16:33 -0500

    Add custom liquidprompt repo and installation in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+
+# Dotfiles
 echo "==> Linking Files"
 if [ ! -d ~/.config ]; then
 	ln -s `pwd`/.config ~/.config
@@ -16,4 +18,27 @@ if [ ! -f ~/.vimrc ]; then
 	ln -s `pwd`/.vimrc ~/.vimrc
 else
 	echo ".vimrc exists, skipping"
+fi
+
+# Submodules
+echo "==> Initializing submodules"
+git submodule init
+git submodule update
+
+# Liquidprompt
+LIQUIDPROMPT_FILE=`pwd`/liquidprompt/liquidprompt
+echo "==> Adding liquidprompt"
+
+if [ -f "$LIQUIDPROMPT_FILE" ]; then
+	if grep -q "$LIQUIDPROMPT_FILE" ~/.bashrc; then
+		echo "already addded to .bashrc, skipping"
+	else
+		echo "liquidprompt not found in ~/.bashrc, adding"
+		echo "" >> ~/.bashrc
+		echo "#adding liquid prompt from dotfiles" >> ~/.bashrc
+		echo "source \"$LIQUIDPROMPT_FILE\"" >> ~/.bashrc
+		echo "" >> ~/.bashrc
+	fi
+else
+	echo "liquidprompt not found, skipping"
 fi
